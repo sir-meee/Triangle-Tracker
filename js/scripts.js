3e5c73c92401c0ec8d311d584ffb3679d1b346fd
sir-meee <sammymbevi@gmail.com>
2018-09-16 21:11:22 +0300

    add array of triangle sides

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
 var length = parseInt(prompt("Enter the length"));
 var height = parseInt(prompt("Enter the height"));
 var hypotenuse = parseInt(prompt("Enter the hypotenuse"));
+var triangle = [length, height, hypotenuse];
