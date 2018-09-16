1bfaef140030f19cd3f476599166e93613687cad
sir-meee <sammymbevi@gmail.com>
2018-09-16 21:17:10 +0300

    add control flows and logical operators in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,15 @@ var length = parseInt(prompt("Enter the length"));
 var height = parseInt(prompt("Enter the height"));
 var hypotenuse = parseInt(prompt("Enter the hypotenuse"));
 var triangle = [length, height, hypotenuse];
+    if (length===height && height===hypotenuse && hypotenuse===length) {
+      alert("Equilateral Triangle");
+}
+    else if (length===height || length===hypotenuse|| length===hypotenuse ) {
+      alert("Isosceles Triangle");
+  }
+    else if (length!==height && height!==hypotenuse && length!=hypotenuse) {
+      alert("Scalene Triangle");
+  }
+    else if ((length+height)<=hypotenuse || (length+hypotenuse)<=height || (height+hypotenuse)<=length) {
+      alert("NOT a Triangle");
+}
